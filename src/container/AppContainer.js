6a70df560bab290503e5a1a4e91daf8cb76f55cc
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-23 21:07:59 +0000

    bugs

--- src/container/AppContainer.js
+++ src/container/AppContainer.js
@@ -1,56 +1,54 @@
-import React from 'react';
-import App from '../components/App';
-import fetch from 'fetch';
+import React from "react";
+import App from "../components/App";
+import fetch from "node-fetch";
 
-const FIXER_LATEST_PATH = 'http://api.fixer.io/latest';
+const FIXER_LATEST_PATH = "http://api.fixer.io/latest";
 
 class AppContainer extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			isFetching: false
+  constructor() {
+    super();
+    this.state = {
+      isFetching: false,
       latestRates: []
-		};
-	}
+    };
+  }
 
-	async componentDidMount() {
-		try {
-			this.setState({
-				isFetching: true
-			});
+  async componentDidMount() {
+    try {
+      this.setState({
+        isFetching: true
+      });
 
-			let response = await fetch(FIXER_LATEST_PATH);
-			if (!response.ok) {
-				throw new Error(`${response.status} ${response.statusText}`);
-			}
+      let response = await fetch(FIXER_LATEST_PATH);
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
       let json = await response.json();
       let latestRates = {
         base: json.base,
         date: json.date,
-        rates = []
+        rates: []
+      };
+      for (let country in json.rates) {
+        latestRates.rates.push({ country, rate: json.rates[country] });
       }
-      for (country in json.rates) {
-        latestRates.rates.push({country, rate: json.rates[country]});
-      }
-			console.log(latestRates);
+      console.log(latestRates);
       this.setState({
-				isFetching: false,
+        isFetching: false,
         latestRates
-			});
+      });
+    } catch (err) {
+      console.error(err);
+      this.setState({
+        isFetching: false
+      });
+    }
+  }
 
-		} catch (err) {
-			console.error(err);
-			this.setState({
-				isFetching: false
-			});
-		}
-	}
-
-	render() {
-		if (this.state.isFetching)
-			return <p>Loading...</p>
-		return <App latestRates={this.state.latestRates}/>;
-	}
+  render() {
+    if (this.state.isFetching) return <p>Loading...</p>;
+    return <App latestRates={this.state.latestRates} />;
+  }
 }
 
 export default AppContainer;
