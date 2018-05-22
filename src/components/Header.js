272ad43e4a69332894b259da58e13f6571a173be
anne <richardson.ae@gmail.com>
2018-05-22 11:54:37 -0500

    Updates app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render(){
     return (
       <React.Fragment>
-        <h1>Currency Rate App</h1>
+        <h1>React Currency Rate App</h1>
         <p>Enter Euros and get back the rates for other currencies.</p>
       </React.Fragment>
     )
