94bd2cae5ca0d9517098cc4becbe4ab106c6f967
anne <richardson.ae@gmail.com>
2018-05-21 20:38:33 -0500

    Removes temp API component from main file

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../App.css';
-import FixerClient from './FixerClient'
 import Header from './Header'
 import Form from './Form'
 import CurrencyRates from './CurrencyRates'
@@ -20,8 +19,6 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        Hello World
-        <FixerClient />
         <Header />
         <Form />
         { this.state.rates ? <CurrencyRates data={this.state.rates}/> : "" }
