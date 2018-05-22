c561377c3975d3e92f3576384eeae5d6be760085
anne <richardson.ae@gmail.com>
2018-05-21 20:34:12 -0500

    Builds Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import FixerClient from './FixerClient'
+import Header from './Header'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="container-fluid">
         Hello World
         <FixerClient />
+        <Header />
       </div>
     );
   }
